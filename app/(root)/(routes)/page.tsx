db9e1610b614435865692cd641dab7339f18ee87
Sachin-God <113123578+Sachin-God@users.noreply.github.com>
2025-11-11 01:07:25 +0530

    Search Functionality and Persona Card and Pages

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,13 +1,37 @@
 import Categories from "@/Components/Categories";
+import PersonaPage from "@/Components/PersonaPage";
 import SearchInput from "@/Components/SearchInput";
 import client from "@/lib/prismadb";
 
-export default async function Home() {
+interface RootPageParams {
+  searchParams: {
+    categoryId: string;
+    name: string;
+  }
+}
+
+export default async function Home({ searchParams }: RootPageParams) {
+  const { categoryId, name } = await searchParams;
+  const data = await client.persona.findMany({
+    where: {
+      categoryId: categoryId,
+      name: name
+    },
+    orderBy: {
+      createdAt: "desc"
+    },
+    include: {
+      _count: {
+        select: { messages: true }
+      }
+    }
+  });
   const categories = await client.category.findMany()
   return (
     <div className="h-full p-4 space-y-2 text-primary">
       <SearchInput />
       <Categories data={categories} />
+      <PersonaPage data={data} />
     </div>
   );
 }
