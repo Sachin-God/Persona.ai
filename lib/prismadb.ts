19a603a7b9bcc23f1721d00efdc4d90e9b5c3e01
Sachin-God <113123578+Sachin-God@users.noreply.github.com>
2025-11-16 01:00:25 +0530

    changed client

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,9 +1,8 @@
-// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { client: PrismaClient };
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const client =
-  globalForPrisma.client || new PrismaClient();
+  globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.client = client;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = client;
